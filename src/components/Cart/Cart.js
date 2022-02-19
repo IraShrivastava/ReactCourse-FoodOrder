ab71ef2dbe3f9376a09ecfbb892d73c72b7bd134
IraShrivastava <ira.shrivastava0211@gmail.com>
2022-02-19 21:49:14 +0530

    initial commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,103 @@
+import React, { useContext, useState } from 'react'
+import Modal from '../UI/Modal'
+import styles from './Cart.module.css'
+import CartContext from '../../store/cart-context'
+import CartItem from './CartItem'
+import Checkout from './Checkout'
+
+const Cart = (props) =>{
+
+    const [isCheckout, setIsCheckout] = useState(false)
+    const [isSubmitting, setIsSubmitting ] = useState(false)
+    const [didSubmit, setDidSubmit ] = useState(false)
+
+    const cartCtx = useContext(CartContext)
+    console.log(cartCtx)
+    const totalAmount = `₹${cartCtx.totalAmount.toFixed(2)}`
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = (id) =>{
+        cartCtx.removeItem(id)
+    }
+
+    const cartItemAddHandler = (item) =>{
+        cartCtx.addItem({...item, amount: 1})
+    }
+
+    const orderHandler = () =>{
+        setIsCheckout(true)
+    }
+
+    const submitOrder  = async (userData) =>{
+        setIsSubmitting(true)
+        await fetch('https://react-custom-hook-http-950ae-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json',{
+            method: 'POST',
+            body:JSON.stringify({
+                user: userData,
+                orderedItems: cartCtx.items
+            })
+        })
+        setIsSubmitting(false)
+        setDidSubmit(true)
+        cartCtx.clearCart()
+    }
+
+    const cartItems = (
+        <ul className={styles['cart-items']}>
+            {cartCtx.items.map((item => (
+                <CartItem 
+                    key={item.id} 
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
+            )))}
+        </ul> 
+    )
+
+    const modalActions =(
+        <div className={styles.actions}>
+            <button className={styles['button--alt']} onClick={props.onClose}>Close</button>
+            {hasItems && 
+                <button 
+                    className={styles.button}
+                    onClick={orderHandler}>Order</button>}
+        </div>
+    )
+
+    const cartModalContent = (
+        <React.Fragment>
+            {cartItems}
+            <div className={styles.total}>
+                <span>Total Amount</span>
+                <span>{totalAmount}</span>
+            </div>
+            {isCheckout && <Checkout 
+                            onConfirm={submitOrder}
+                            onCancel={props.onClose} />}
+            {!isCheckout && modalActions}
+        </React.Fragment>
+    )
+
+    const isSubmittingModalContent = <p>Sending order data...</p>
+
+    const didSubmitModalContent = (
+        <React.Fragment>
+            <p>Successfully placed the order!</p>
+            <div className={styles.actions}>
+                <button className={styles.button} onClick={props.onClose}>Close</button>
+            </div>
+        </React.Fragment>
+    )
+    return (
+        <Modal onClose={props.onClose}>
+            {!isSubmitting && !didSubmit && cartModalContent}
+            {isSubmitting && isSubmittingModalContent}
+            {!isSubmitting && didSubmit && didSubmitModalContent}
+        </Modal>
+    )
+}
+
+export default Cart
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import Card from '../UI/Card'
+import styles from './AvailableMeals.module.css'
+import MealItem from './MealItem/MealItem';
+
+
+const AvailableMeals = () => {
+
+    const [meals, setMeals] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [httpError, setHttpError] = useState()
+
+    useEffect(()=>{
+        const fetchMeals = async () =>{
+            setIsLoading(true)
+            const response = await fetch('https://react-custom-hook-http-950ae-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json')
+            if(!response.ok){
+                throw new Error('Something went wrong!')
+            }
+            
+            const responseData = await response.json()
+
+            const loadedMeals = [];
+
+            for (const key in responseData){
+                loadedMeals.push({
+                    id:key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price
+                })
+            }
+            setMeals(loadedMeals)
+            setIsLoading(false)
+        }
+
+        fetchMeals().catch(error=>{
+            setIsLoading(false)
+            setHttpError(error.message)
+        });
+
+    },[])
+
+    if(isLoading){
+        return (
+            <section className={styles.MealsLoading}>
+                <p>Loading...</p>
+            </section>
+        )
+    }
+
+    if(httpError){
+        return (
+            <section className={styles.MealsError}>
+                <p>{httpError}</p>
+            </section>
+        )
+    }
+
+    const mealsList = meals.map(meal =>(
+        <MealItem 
+            id={meal.id}
+            key={meal.id} 
+            name={meal.name}
+            description={meal.description}
+            price={meal.price} 
+        />
+    ))
+    return (
+        <section className={styles.meals}>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
+        </section>
+    )
+}
+
+export default AvailableMeals;
